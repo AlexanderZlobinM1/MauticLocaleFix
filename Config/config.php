4cdd68b0f76b1aec71c4b1f4f368ba992044c3bb
AlexanderZlobinM1 <alexander.zlobin@m1.rs>
2026-06-08 17:13:13 +0200

    Add interface translations

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Mautic Locale Fix',
     'description' => 'Regional UI settings for Mautic without core patches.',
-    'version'     => '1.0.1',
+    'version'     => '1.0.2',
     'author'      => 'Sales Snap',
     'services'    => [
         'events' => [
